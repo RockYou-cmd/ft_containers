0c0bfb7a1f2dcb102d188317b69ac11359a133d9
Alaaeddine El korchi <ael-korc@e2r10p11.1337.ma>
2023-02-02 08:03:37 +0100

    resize function added

--- containers/ft_vector.h
+++ containers/ft_vector.h
@@ -6,7 +6,7 @@
 /*   By: ael-korc <ael-korc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/02 15:57:07 by ncolomer          #+#    #+#             */
-/*   Updated: 2023/01/30 23:44:38 by ael-korc         ###   ########.fr       */
+/*   Updated: 2023/02/02 07:49:41 by ael-korc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,21 +42,39 @@ namespace ft
 			bdata = _allocator.allocate(10);
 			for (int i = 0; i < nb; i ++)
 				_allocator.construct(bdata + i, val);
-			edata = bdata + (nb - 1);
+			edata = bdata + nb;
 			
 		}
-		// Vector& operator=(const Vector& other)
-		// {
-			
-		// };
-		// void reserve(size_type nsize)
-		// {
-		// 	if (nsize > cap)
-		// 	{
-				
-		// 	}
-		// }
+		Vector& operator=(const Vector& other)
+		{
+			// std::cout << "here----------\n";
+			if (this != &other && other.vcap > 0)
+			{
+				if (vcap > 0)
+				{
+					for(int i = 0; i < vsize; i ++)
+						_allocator.destroy(bdata + i);
+					_allocator.deallocate(bdata, vcap);
+				}
+				bdata = _allocator.allocate(other.vcap);
+				for (int i = 0; i < other.vsize; i ++)
+					_allocator.construct(bdata + i, *(other.bdata + i));
+				vcap = other.vcap;
+				vsize = other.vsize;
+			}
+			return *this;
+		}
+		// begin()
+		iterator begin()
+		{
+			return iterator(bdata);
+		}
 		
+		// end()
+		iterator end()
+		{
+			return iterator(end);
+		}
 		// push_back	
 		void push_back (const value_type& val)
 		{
@@ -84,10 +102,88 @@ namespace ft
 		
 		void pop_back ()
 		{
-			_allocator.destroy(edata);
+			_allocator.destroy(edata - 1);
 			vsize--;
 		}
+
+		void resize(size_type n, value_type val = value_type())
+		{
+			if (n < 0) /// prob here n == cap
+				return;
+			if (n > vcap)
+			{
+				pointer tmp = _allocator.allocate(n);
+				for (int i = 0; i < n; i ++)
+				{
+					if (i < vsize)
+						_allocator.construct(tmp + i, *(bdata + i));
+					else
+						_allocator.construct(tmp + i, val);
+				}
+				for (int i = 0; i < vsize; i ++)
+					_allocator.destroy(bdata + i);
+				_allocator.deallocate(bdata, vcap);
+				bdata = tmp;
+				vsize = n;
+				vcap = n;
+			}
+			else if (n == 0)
+			{
+				for (int i = 0; i < vsize; i ++)
+					_allocator.destroy(bdata + i);
+				vsize = 0;
+			}
+			else if (n <= vcap)
+			{
+				for (int i = 0; i < vcap; i ++)
+				{
+					if (i < vsize && i < n)
+						continue;
+					if (i < n)
+						_allocator.construct(bdata + i, val);
+				}
+				vsize = n;
+			}
+				
+		}
 		
+	 	iterator insert()
+		{
+			
+		}
+		
+		size_type capacity()
+		{
+			return vcap;
+		}
+
+
+
+
+
+
+
+
+
+		
+		void print()
+		{
+			for (int i = 0; i < vsize; i ++)	
+				std::cout << bdata[i] << std::endl;
+			std::cout << "capacity :  " << vcap << std::endl;
+			std::cout << "size :  " << vsize << std::endl;
+		}
+		private:
+			size_type		vsize;
+			size_type 		vcap;
+			allocator_type _allocator;
+			pointer			bdata;
+			pointer			edata;
+	};
+}
+
+#endif
+
 		// void push_front (const value_type& val)
 		// {
 			// std::cout << *(bdata);
@@ -119,20 +215,3 @@ namespace ft
 		// 	}
 		// 	vsize++;
 		// }
-		void print()
-		{
-			for (int i = 0; i < vsize; i ++)	
-				std::cout << bdata[i] << std::endl;
-			std::cout << "capacity :  " << vcap << std::endl;
-		}
-		size_type capacity() {return vcap;}
-		private:
-			size_type		vsize;
-			size_type 		vcap;
-			allocator_type _allocator;
-			pointer	bdata;
-			pointer	edata;
-	};
-}
-
-#endif
--- iterators/v_iterator.h
+++ iterators/v_iterator.h
@@ -13,7 +13,50 @@ namespace ft
 		typedef T* 								pointer;
 		typedef T& 								reference;
 		typedef std::random_access_iterator_tag iterator_category;
-		v_iterator(){};
+		v_iterator(pointer vdata)
+		{
+			it = vdata;
+		};
+		value_type operator*()
+		{
+			return (*it);
+		}
+		
+		value_type operator++(int)
+		{
+			it += 1;
+			return *(it - 1);
+		}
+
+		value_type operator++()
+		{
+			it += 1;
+			return *it;
+		}
+
+		value_type operator--(int)
+		{
+			it -= 1;
+			return *(it + 1);
+		}
+
+		value_type operator--()
+		{
+			it -= 1;
+			return *it;
+		}
+
+		bool operator==(const pointer& other)
+		{
+			return *it == *(other->it);
+		}
+
+		bool operator!=(const pointer& other)
+		{
+			return *it != *(other->it);
+		}
+		private :
+			pointer it;
 	};
 }
 
