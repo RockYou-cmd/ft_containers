5e5d74e8535fae41f70a2036bc9bf7e4303d387a
Alaaeddine El korchi <elkorchi.alae@gmail.com>
2023-02-09 23:59:36 +0100

    i added iterator treat v2

--- containers/ft_vector.h
+++ containers/ft_vector.h
@@ -6,7 +6,7 @@
 /*   By: ael-korc <ael-korc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/02 15:57:07 by ncolomer          #+#    #+#             */
-/*   Updated: 2023/02/08 20:11:30 by ael-korc         ###   ########.fr       */
+/*   Updated: 2023/02/09 17:22:03 by ael-korc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ namespace ft
 			typedef 			std::size_t 					size_type;
 			typedef 			std::size_t 					difference_type;
 			typedef 			value_type& 					reference;
-			typedef 			const value_type& 				const_reference;
+			typedef 			const value_type& 		1		const_reference;
 			typedef typename 	allocator_type::pointer 		pointer;
 			typedef typename 	allocator_type::const_pointer 	const_pointer;
 			typedef 			ft::v_iterator<T> 				iterator;
--- iterators/iterators.h
+++ iterators/iterators.h
@@ -6,6 +6,26 @@
 #include <iterator>
 #include <iostream>
 
+template <typename Iterator>
+struct iterator_traits
+{
+    typedef typename Iterator::reference reference;
+    typedef typename Iterator::pointer pointer;
+    typedef typename Iterator::difference_type difference_type;
+    typedef typename Iterator::value_type value_type;
+    typedef typename Iterator::iterator_category iterator_category;
+};
+
+template <typename T>
+struct iterator_traits<T*>
+{
+    typedef std::random_access_iterator_tag iterator_category;
+    typedef T value_type;
+    typedef ptrdiff_t difference_type;
+    typedef T* pointer;
+    typedef T& reference;
+};
+
 
 
 #endif //ITERATORS
